20fe9e217e935d4728fde41038ecabebed283e8e
Anton Danilkin <afdw@yandex.ru>
2017-05-28 16:40:30 +0300

    Fix ILaserTarget

--- api/buildcraft/api/mj/ILaserTarget.java
+++ api/buildcraft/api/mj/ILaserTarget.java
@@ -1,26 +1,28 @@
-/** Copyright (c) 2011-2015, SpaceToad and the BuildCraft Team http://www.mod-buildcraft.com
- *
- * The BuildCraft API is distributed under the terms of the MIT License. Please check the contents of the license, which
- * should be located as "LICENSE.API" in the BuildCraft source code distribution. */
+/*
+ * Copyright (c) 2017 SpaceToad and the BuildCraft team
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+ */
+
 package buildcraft.api.mj;
 
-/** This interface should be defined by any Tile which wants to receive power from BuildCraft lasers.
- *
- * The respective Block MUST implement ILaserTargetBlock! */
+/**
+ * This interface should be defined by any Tile which wants to receive power from BuildCraft lasers.
+ * <p>
+ * The respective Block MUST implement {@link ILaserTargetBlock}!
+ */
 public interface ILaserTarget {
-
-    /** Returns true if the target currently needs power. For example, if the Advanced Crafting Table has work to do.
+    /**
+     * Returns power that the target currently needs. Returns 0 if no power is needed (tile has nothing to do)
      *
-     * @return true if needs power */
-    boolean requiresLaserPower();
+     * @return The number of micro Minecraft Joules
+     */
+    long getRequiredLaserPower();
 
-    /** Transfers power from the laser to the target.
+    /**
+     * Transfers power from the laser to the target.
      *
-     * @param microJoules The number of micro Minecraft Joules to accept */
+     * @param microJoules The number of micro Minecraft Joules to accept
+     */
     void receiveLaserPower(long microJoules);
-
-    /** Return true if the Tile Entity object is no longer a valid target. For example, if its been invalidated.
-     *
-     * @return true if no longer a valid target object */
-    boolean isInvalidTarget();
 }
