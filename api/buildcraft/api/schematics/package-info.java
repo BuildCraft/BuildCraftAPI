ae3f8dd51bf03afa6f249783c3dfe6255afedadc
AEnterprise <aenterprise2@gmail.com>
2017-07-07 21:56:08 +0200

    bump api versions

---
+++ api/buildcraft/api/schematics/package-info.java
@@ -0,0 +1,5 @@
+
+@API(apiVersion = "1.0", owner = "BuildCraftAPI|core", provides = "BuildCraftAPI|schematics")
+package buildcraft.api.schematics;
+
+import net.minecraftforge.fml.common.API;
---
+++ api/buildcraft/api/template/package-info.java
@@ -0,0 +1,5 @@
+
+@API(apiVersion = "1.0", owner = "BuildCraftAPI|core", provides = "BuildCraftAPI|template")
+package buildcraft.api.template;
+
+import net.minecraftforge.fml.common.API;
