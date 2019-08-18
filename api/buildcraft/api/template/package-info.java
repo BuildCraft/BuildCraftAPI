d11179219234896fd86554526a01c5c113cbdd82
AlexIIL <AlexIIL@users.noreply.github.com>
2019-08-18 17:57:15 +0100

    Update package-info.java

--- api/buildcraft/api/template/package-info.java
+++ api/buildcraft/api/template/package-info.java
@@ -1,5 +1,5 @@
 
-@API(apiVersion = "1.0", owner = "buildcraftapi_core", provides = "buildcraftapi_template")
+@API(apiVersion = "1.0", owner = "buildcraftapi_core", provides = "buildcraftapi_templates")
 package buildcraft.api.template;
 
 import net.minecraftforge.fml.common.API;
